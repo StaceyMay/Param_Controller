92ef0ca4d28ef405c8fa26f056fc9a9833125de1
staceymay <missstaceymay@gmail.com>
2016-11-13 09:47:18 -0800

    Game 2 complete

--- app/controllers/params_controller.rb
+++ app/controllers/params_controller.rb
@@ -21,4 +21,36 @@ class ParamsController < ApplicationController
       end
     end
 
+    def url_params
+      @message = params[:message]
+      @message_2 = params[:message_2]
+    end
+
+    def new_game
+      winning_number = 44
+      @guess = params[:guess]
+      if @guess.to_i > winning_number
+        @x =  "Guess a little lower"
+      elsif @guess.to_i < winning_number
+        @x = "Guess a little higher"
+      else
+        @x = "Winner Winner Chicken Dinner"
+      end
+    end
+
+    def form_display
+
+    end
+
+    def form_result
+        winning_number = 44
+    @guess = params[:guess]
+      if @guess.to_i > winning_number
+        @x =  "Guess a little lower"
+      elsif @guess.to_i < winning_number
+        @x = "Guess a little higher"
+      else
+        @x = "Winner Winner Chicken Dinner!!"
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   get "/param_test", to: "params#param_query"
   get "/param_game", to: "params#param_game"
   get "/param_num", to: "params#param_num"
+
+  get "/new/:guess", to: "params#new_game"
+
+  # http://localhost:3000/param_game?name=ashley
+  get "/url_parameter/:message/:message_2", to: "params#url_params"
+
+  get "/form_display", to: "params#form_display"
+  post "/form_result", to: "params#form_result"
 end
